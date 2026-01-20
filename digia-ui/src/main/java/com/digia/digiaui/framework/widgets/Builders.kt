fafe7136d3da0b50f3ffa22968d3da2b4102d02f
Arun kumar <arunkumar@Aruns-MacBook-Air.local>
2026-01-20 20:49:33 +0530

    webview buggy

--- digia-ui/src/main/java/com/digia/digiaui/framework/widgets/Builders.kt
+++ digia-ui/src/main/java/com/digia/digiaui/framework/widgets/Builders.kt
@@ -14,50 +14,56 @@ import com.digia.digiaui.framework.models.VWNodeData
 
 /** Register all built-in widgets with the registry */
 fun DefaultVirtualWidgetRegistry.registerBuiltInWidgets() {
-    // Register Text widget
-    register("digia/text", ::textBuilder)
+        // Register Text widget
+        register("digia/text", ::textBuilder)
 
-    // Register layout widgets
-    register("digia/column", ::columnBuilder)
-    register("digia/row", ::rowBuilder)
-    //    register("digia/stack", ::stackBuilder)
+        // Register layout widgets
+        register("digia/column", ::columnBuilder)
+        register("digia/row", ::rowBuilder)
+        //    register("digia/stack", ::stackBuilder)
 
-    // Register list widget
-    register("digia/listView", ::listViewBuilder)
+        // Register list widget
+        register("digia/listView", ::listViewBuilder)
 
-    register("digia/streamBuilder", ::streamBuilderBuilder)
+        register("digia/streamBuilder", ::streamBuilderBuilder)
 
-    register("digia/conditionalBuilder", ::conditionalBuilder)
-    register("digia/conditionalItem", ::conditionalItemBuilder)
+        register("digia/conditionalBuilder", ::conditionalBuilder)
+        register("digia/conditionalItem", ::conditionalItemBuilder)
 
-    // Register Scaffold widget (commented out for now)
-    register("fw/scaffold", ::scaffoldBuilder)
-    // Register AppBar widget
-    register("digia/appBar", ::appBarBuilder)
-    register("fw/appBar", ::appBarBuilder)
-    register("digia/circularProgressBar", ::circularProgressBarBuilder)
-    register("digia/futureBuilder", ::futureBuilder)
-    register("digia/lottie", ::lottieBuilder)
-    register("digia/linearProgressBar", ::linearProgressBarBuilder)
-    register("digia/textFormField", ::textFormFieldBuilder)
-    register("digia/videoPlayer", ::videoPlayerBuilder)
-    register("digia/button", ::buttonBuilder)
-    register("digia/image", ::imageBuilder)
-    register("digia/container", ::containerBuilder)
-    register("digia/carousel", ::carouselBuilder)
-    register("digia/wrap", ::wrapBuilder)
-    register("digia/opacity", ::opacityBuilder)
-    register("opacity", ::opacityBuilder)
-    register("fw/opacity", ::opacityBuilder)
+        // Register Scaffold widget (commented out for now)
+        register("fw/scaffold", ::scaffoldBuilder)
+        // Register AppBar widget
+        register("digia/appBar", ::appBarBuilder)
+        register("fw/appBar", ::appBarBuilder)
+        register("digia/circularProgressBar", ::circularProgressBarBuilder)
+        register("digia/futureBuilder", ::futureBuilder)
+        register("digia/lottie", ::lottieBuilder)
+        register("digia/linearProgressBar", ::linearProgressBarBuilder)
+        register("digia/textFormField", ::textFormFieldBuilder)
+        register("digia/videoPlayer", ::videoPlayerBuilder)
+        register("digia/button", ::buttonBuilder)
+        register("digia/image", ::imageBuilder)
+        register("digia/container", ::containerBuilder)
+        register("digia/carousel", ::carouselBuilder)
+        register("digia/wrap", ::wrapBuilder)
+        register("digia/opacity", ::opacityBuilder)
+        register("digia/carousel", ::carouselBuilder)
+        register("digia/wrap", ::wrapBuilder)
+        register("digia/opacity", ::opacityBuilder)
+        register("opacity", ::opacityBuilder)
+        register("fw/opacity", ::opacityBuilder)
 
-    register("digia/stack", ::stackBuilder)
-    register("digia/styledHorizontalDivider", ::styledHorizontalDividerBuilder)
+        register("digia/webView", ::webViewBuilder)
+        register("fw/webView", ::webViewBuilder)
 
-    register("fw/sizedBox", ::sizedBoxBuilder)
-    register("fw/sized_box", ::sizedBoxBuilder)
-    register("digia/gridView", ::dummyBuilder)
-    register("digia/richText", ::dummyBuilder)
-    register("digia/calendar", ::dummyBuilder)
+        register("digia/stack", ::stackBuilder)
+        register("digia/styledHorizontalDivider", ::styledHorizontalDividerBuilder)
+
+        register("fw/sizedBox", ::sizedBoxBuilder)
+        register("fw/sized_box", ::sizedBoxBuilder)
+        register("digia/gridView", ::dummyBuilder)
+        register("digia/richText", ::dummyBuilder)
+        register("digia/calendar", ::dummyBuilder)
 }
 
 fun dummyBuilder(
@@ -65,13 +71,13 @@ fun dummyBuilder(
         parent: VirtualNode?,
         registry: VirtualWidgetRegistry
 ): VirtualNode {
-    return VWDummy(
-            refName = nodeData.refName,
-            commonProps = nodeData.commonProps,
-            parent = parent,
-            parentProps = nodeData.parentProps,
-            props = nodeData.props
-    )
+        return VWDummy(
+                refName = nodeData.refName,
+                commonProps = nodeData.commonProps,
+                parent = parent,
+                parentProps = nodeData.parentProps,
+                props = nodeData.props
+        )
 }
 
 class VWDummy(
@@ -89,8 +95,8 @@ class VWDummy(
                 parentProps = parentProps
         ) {
 
-    @Composable
-    override fun Render(payload: RenderPayload) {
-        Text(text = "Dummy Widget Rendered", modifier = Modifier.buildModifier(payload))
-    }
+        @Composable
+        override fun Render(payload: RenderPayload) {
+                Text(text = "Dummy Widget Rendered", modifier = Modifier.buildModifier(payload))
+        }
 }
